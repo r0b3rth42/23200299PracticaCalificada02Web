dfab6596e63adb7cb633a3c1af8b2674975e6e69
unknown <irazabarob44@gmail.com>
2025-11-29 16:55:03 -0500

    redirigin a login

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,28 +1,37 @@
 const routes = [
+  // LOGIN
+  {
+    path: '/login',
+    component: () => import('src/pages/auth/LoginForm.vue'),
+  },
+
+  // REDIRECCIÓN AUTOMÁTICA desde "/" → "/login"
+  {
+    path: '/',
+    redirect: '/login'
+  },
+
+  // MAIN LAYOUT (solo al entrar a /home)
   {
     path: '/home',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { 
-        path: '', 
-        component: () => import('pages/IndexPage.vue') 
+        path: '',
+        component: () => import('pages/IndexPage.vue')
       },
       {
-        path: '/digimon',
+        path: '/digimon',  
         component: () => import('src/pages/digimon/DigimonCards.vue'),
       }
     ],
   },
-   {
-    path: '/',
-    component: () => import('src/pages/auth/LoginForm.vue'),
-  },
-  // Always leave this as last one,
-  // but you can also remove it
+
+  // 404
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
-  },
+  }
 ]
 
 export default routes
